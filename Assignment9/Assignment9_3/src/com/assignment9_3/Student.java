016c495de4877aaa56efd0cf4086315b18993651
kd1-80095 <aniketjadhav241299@.com>
2023-10-19 18:18:23 +0530

    Assignment9_Uploaded

---
+++ Assignment9/Assignment9_3/src/com/assignment9_3/Student.java
@@ -0,0 +1,65 @@
+package com.assignment9_3;
+
+public class Student implements Comparable <Student>{
+
+	private int roll;
+	private String name;
+	private String city;
+	private double marks;
+	
+	public Student() {
+		this.roll = 0;
+		this.name = "";
+		this.city = "";
+		this.marks = 0;
+	}
+	
+	public Student(int roll, String name, String city, double marks) {
+		this.roll = roll;
+		this.name = name;
+		this.city = city;
+		this.marks = marks;
+	}
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+
+	
+	@Override
+	public int compareTo(Student other) {
+		
+		return 0;
+	}
+	
+	
+	
+}
